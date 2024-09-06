7cdc855d09f7bf65913e64dc7e85748ace582e55
Nidhi Kumari <kumarinidhi0594@gmail.com>
2024-09-06 21:56:24 +0530

    Freature branch (#3)
    * Adding
    
    * Services
    
    ---------
    
    Co-authored-by: Nidhi Kumari <“nikumari@extremenetworks.com”>

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+  encapsulation: ViewEncapsulation.None // Disable encapsulation for testing
+})
+export class HeaderComponent { }
+
